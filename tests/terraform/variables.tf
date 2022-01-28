de6cae1abd00ab34c1f5f0ec90fd8fbcb2e04359
Yomi Bazuaye <yomi.bazuaye@craft.co>
2022-01-28 10:26:35 +0000

    added backend and variables config files

---
+++ tests/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+}
+
+variable "secret_key" {
+}
+
+variable "ops_access_key" {
+}
+
+variable "ops_secret_key" {
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
